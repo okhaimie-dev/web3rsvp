24504f6444359c774bb2810597d4598c16be8e01
Tony Stark <afemiadom@icloud.com>
2022-10-04 20:57:46 -0500

    Added deploy mechanism

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    }
+  }
 };
